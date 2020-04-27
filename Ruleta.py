55d1ed96104e28d2a7fbf3a3c26a4c1d63832c3b
Damian <dbdamix@gmail.com>
2020-04-26 21:55:44 -0300

    Tp1

--- Ruleta.py
+++ Ruleta.py
@@ -18,22 +18,6 @@ lista=[]
 
 
 def main():
-    Listas()
-    print("Numeros:")
-    print(lista)
-    TablaProm()
-    TablaPromProm()
-    TablaFrecRel()
-    TablaFrecAbs()
-    TablaVarianza()
-    TablaVarProm()
-    TablaDesviacion()
-    TablaDesvProm()
-
-
-#--------------------------------------------------------------------------------------------------------------------
-
-def Listas():
     for i in range(n):
         lista.append(randint(a,b))
         prom.append(np.mean(lista))
@@ -44,37 +28,86 @@ def Listas():
             varianza.append(np.var(lista))
             varianza_promedio.append(np.var(prom))
 
+    print("Numeros:")
+    print(lista)
+    Graficar(lista,prom, promedio_promedio, varianza, varianza_promedio, desviacion,desviacion_promedio)
+    GraficarFrec(lista)
+
+
+#--------------------------------------------------------------------------------------------------------------------
+
+
+
 
 def adjust_y_axis(x, pos):
     return x / (len(lista) * 1.0)
 
 
-def TablaProm():
-    promEsp = np.mean(lista)
-    plt.plot(list(range(len(prom))), prom, color="red",label="Promedio")
+def Graficar(lis,p, pp, v, vp, des,desp):
+    #Promedio
+    promEsp = np.mean(lis)
+    plt.plot(list(range(len(p))), p, color="red",label="Promedio")
     plt.plot([0, n], [promEsp, promEsp], label="Promedio Esperada", color="blue")
     plt.title("Promedio")
     plt.xlabel('Nro de tiradas')
     plt.ylabel('Promedio')
     plt.legend()
     plt.show()
-
-
-def TablaPromProm():
-    promEsp = np.mean(lista)
+    #PromProm
+    promEsp = np.mean(lis)
     plt.title("Promedio de los Promedios")
     plt.xlabel('Nro de tiradas')
     plt.ylabel('Promedios')
-    plt.plot(promedio_promedio, label="Promedio", color="red")
+    plt.plot(pp, label="Promedio", color="red")
     plt.plot([0, n], [promEsp, promEsp], label="Promedio Esperado", color="blue")
+    plt.legend()
+    plt.show()
+    #Varianza
+    varEsp = np.var(lis)
+    plt.title('Varianza')
+    plt.xlabel('Nro de tiradas')
+    plt.ylabel('varianza')
+    plt.plot(v, label="Varianza", color="red")
+    plt.plot([0, n], [varEsp, varEsp], label="Varianza Esperada", color="blue")
+    plt.legend()
+    plt.show()
+    #VarProm
+    varEsp = np.var(p)
+    plt.title('Varianza de los Promedios')
+    plt.xlabel('Nro de tiradas')
+    plt.ylabel('varianza promedio')
+    plt.plot(vp, label="Varianza", color="red")
+    plt.plot([0, n], [varEsp, varEsp], label="Varianza Esperada", color="blue")
+    plt.legend()
+    plt.show()
+    #Desviacion
+    desvEsp = np.std(lis)
+    plt.title('Desviacion')
+    plt.xlabel('Nro de tiradas')
+    plt.ylabel('desviacion')
+    plt.plot(des, label="Desviacion", color="red")
+    plt.plot([0, n], [desvEsp, desvEsp], label="Desviacion Esperada", color="blue")
+    plt.legend()
+    plt.show()
+    #DesvProm
+    desvPromEsp=np.std(p)
+    plt.title('Desviacion de los Promedios')
+    plt.xlabel('Nro de tiradas')
+    plt.ylabel('desviaciones')
+    plt.plot(desp, label="Desviacion", color="red")
+    plt.plot([0, n], [desvPromEsp, desvPromEsp], label="Desviacion Esperada", color="blue")
     plt.legend()
     plt.show()
 
 
-def TablaFrecRel():
-    frEsp = 1 / len(lista)
+
+
+
+def GraficarFrec(lis):
+    #FR
+    frEsp = 1 / len(lis)
     fig, ax = plt.subplots()
-    ax.hist(lista, bins=b + 1, edgecolor ="red")
+    ax.hist(lis, bins=b + 1, edgecolor ="red")
     ax.yaxis.set_major_formatter(tick.FuncFormatter(adjust_y_axis))
     ax.axhline(y=frEsp * 1000, color="blue", label="Frecuencia Relativa Esperada")
     fig.tight_layout()
@@ -83,12 +116,10 @@ def TablaFrecRel():
     plt.ylabel('fr')
     plt.legend()
     plt.show()
-
-
-def TablaFrecAbs():
-    fabsEsp = n / len(lista)
+    #FA
+    fabsEsp = n / len(lis)
     fig, ax = plt.subplots()
-    ax.hist(lista, bins=b + 1, edgecolor="red")
+    ax.hist(lis, bins=b + 1, edgecolor="red")
     ax.axhline(y=fabsEsp, color="blue", label="Frecuencia Absoluta Esperada")
     fig.tight_layout()
     plt.title("Frecuencia Absoluta")
@@ -98,51 +129,9 @@ def TablaFrecAbs():
     plt.show()
 
 
-def TablaVarianza():
-    varEsp = np.var(lista)
-    plt.title('Varianza')
-    plt.xlabel('Nro de tiradas')
-    plt.ylabel('varianza')
-    plt.plot(varianza, label="Varianza", color="red")
-    plt.plot([0, n], [varEsp, varEsp], label="Varianza Esperada", color="blue")
-    plt.legend()
-    plt.show()
-
-
-def TablaVarProm():
-    varEsp = np.var(prom)
-    plt.title('Varianza de los Promedios')
-    plt.xlabel('Nro de tiradas')
-    plt.ylabel('varianza promedio')
-    plt.plot(varianza_promedio, label="Varianza", color="red")
-    plt.plot([0, n], [varEsp, varEsp], label="Varianza Esperada", color="blue")
-    plt.legend()
-    plt.show()
-
-
-def TablaDesviacion():
-    desvEsp = np.std(lista)
-    plt.title('Desviacion')
-    plt.xlabel('Nro de tiradas')
-    plt.ylabel('desviacion')
-    plt.plot(desviacion, label="Desviacion", color="red")
-    plt.plot([0, n], [desvEsp, desvEsp], label="Desviacion Esperada", color="blue")
-    plt.legend()
-    plt.show()
-
-
-def TablaDesvProm():
-    desvPromEsp=np.std(prom)
-    plt.title('Desviacion de los Promedios')
-    plt.xlabel('Nro de tiradas')
-    plt.ylabel('desviaciones')
-    plt.plot(desviacion_promedio, label="Desviacion", color="red")
-    plt.plot([0, n], [desvPromEsp, desvPromEsp], label="Desviacion Esperada", color="blue")
-    plt.legend()
-    plt.show()
-
 
 main()
+
 
 #------------------------------------------------------------------------------------------------------------------------
 
