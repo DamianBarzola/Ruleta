fa7ee33497f7ae9df10826adaf3049ff22aaed0c
Damian <dbdamix@gmail.com>
2020-05-01 14:59:27 -0300

    -

--- DAlembert.py
+++ DAlembert.py
@@ -36,9 +36,11 @@ def apuestaDinero():
             if estaencolumna():
                 dinero+=apuesta
                 apuesta=apuesta-apuestaMin
+                ganado.append(1)
             else:
                 dinero-=apuesta
                 apuesta=apuesta+apuestaMin
+                ganado.append(0)
         a+=1
     return dineroTotal
 
@@ -50,15 +52,18 @@ def apuestaDineroInfinito():
 
     for a in range(0,tiradas):
         dineroTotalInf.append(dinero)
+
         if dinero<apuesta:
             break
         else:
             if estaencolumna():
                 dinero+=apuesta
                 apuesta=apuestaMin - apuestaMin
+                ganadoInf.append(1)
             else:
                 dinero-=apuesta
                 apuesta=apuesta+apuestaMin
+                ganadoInf.append(0)
 
     return dineroTotalInf
 
@@ -67,24 +72,64 @@ def apuestaDineroInfinito():
 
 
 def graficaDinero():
+    calcularFrec()
     #Dinero Finito
-    #plt.subplot(2, 2, 1)
+    plt.subplot(2, 2, 1)
     plt.xlabel('Nº de tiradas')
     plt.ylabel('Cantidad de Capital')
     plt.title("Capital Acotado ")
     plt.plot([0, len(dineroTotal)], [dineroInicial, dineroInicial], color="b",label="Dinero Inicial")
     plt.plot(dineroTotal, label="Dinero",color="r")
     plt.legend()
-    plt.show()
     #Dinero Infinito
-    #plt.subplot(2, 2, 2)
+    plt.subplot(2, 2, 2)
     plt.xlabel('Nº de tiradas')
     plt.ylabel('Cantidad de Capital')
     plt.title("Capital Infinito a 1000 tiradas")
     plt.plot([0, len(dineroTotalInf)], [dineroInicial, dineroInicial], color="b",label="Dinero Inicial")
     plt.plot(dineroTotalInf, label="Dinero",color="r")
     plt.legend()
+    #Frec de Aciertos acotado
+    plt.subplot(2, 2, 3)
+    plt.xlabel('Nº de tiradas')
+    plt.ylabel('FR de Acietos')
+    plt.title("Frecuencia de Aciertos con Capital Acotado")
+    plt.plot(frec, color="r")
+    #Frec de Aciertos inf
+    plt.subplot(2, 2, 4)
+    plt.xlabel('Nº de tiradas')
+    plt.ylabel('FR de Acietos')
+    plt.title("Frecuencia de Aciertos con Capital Infinito")
+    plt.plot(frecInf, color="r")
     plt.show()
+
+
+
+frec=[]
+frecInf=[]
+
+
+def calcularFrec():
+    c=0
+    cont=0
+    b=0
+    #for a in range(0,len(ganado)):
+    for a in ganado:
+        cont+=1
+        b=b+a
+        c=round(b/cont,2)
+        frec.append(c)
+    c=0
+    cont=0
+    b=0
+    for a in ganadoInf:
+        cont+=1
+        b=b+a
+        c=round(b/cont,2)
+        frecInf.append(c)
+
+
+
 
 
 
@@ -93,7 +138,6 @@ def main():
     apuestaDinero()
     apuestaDineroInfinito()
     graficaDinero()
-
 
 
 
--- Martingala.py
+++ Martingala.py
@@ -19,6 +19,11 @@ lista_dinero2 = []
 dinero_inicial = 1000
 apuesta_minima = 10
 
+frec=[]
+frecInf=[]
+ganadoInf=[]
+ganado=[]
+
 
 def getColor(numero):
     if numero in numeros_rojos:
@@ -41,18 +46,22 @@ def apuesta_dinero():
         if getColor(lista_numeros[-1]) == "rojo":
             dinero += apuesta
             apuesta = apuesta_minima
+            ganado.append(1)
         else:
+            ganado.append(0)
             if getColor(lista_numeros[-1]) == "negro":
                 dinero -= apuesta
+
             else:
                 dinero -= apuesta / 2
+
             apuesta *= 2
 
         count += 1
     return lista_dinero
 
 
-def apuesta_n_veces():
+def apuesta_dineroInf():
     n=1000
     dinero = dinero_inicial
     apuesta = apuesta_minima
@@ -60,9 +69,11 @@ def apuesta_n_veces():
         lista_dinero2.append(dinero)
         lista_numeros2.append(rn.randint(min_n, max_n))
         if getColor(lista_numeros2[-1]) == "rojo":
+            ganadoInf.append(1)
             dinero += apuesta
             apuesta = apuesta_minima
         else:
+            ganadoInf.append(0)
             if getColor(lista_numeros2[-1]) == "negro":
                 dinero -= apuesta
             else:
@@ -71,8 +82,29 @@ def apuesta_n_veces():
     return lista_dinero2
 
 
+def calcularFrec():
+    c=0
+    cont=0
+    b=0
+    #for a in range(0,len(ganado)):
+    for a in ganado:
+        cont+=1
+        b=b+a
+        c=round(b/cont,2)
+        frec.append(c)
+    c=0
+    cont=0
+    b=0
+    for a in ganadoInf:
+        cont+=1
+        b=b+a
+        c=round(b/cont,2)
+        frecInf.append(c)
 
-def plot_caja():
+
+
+def graficaDinero():
+    calcularFrec()
     #Dinero Finito
     plt.subplot(2, 2, 1)
     plt.grid()
@@ -91,13 +123,26 @@ def plot_caja():
     plt.plot(lista_dinero2, label="Dinero",color="r")
     plt.plot([0, len(lista_numeros2)], [dinero_inicial, dinero_inicial],color="b",label="Dinero Inicial")
     plt.legend()
+    #Frec de Aciertos acotado
+    plt.subplot(2, 2, 3)
+    plt.xlabel('Nº de tiradas')
+    plt.ylabel('FR de Acietos')
+    plt.title("Frecuencia de Aciertos con Capital Acotado")
+    plt.plot(frec,color="r")
+    #Frec de Aciertos inf
+    plt.subplot(2, 2, 4)
+    plt.xlabel('Nº de tiradas')
+    plt.ylabel('FR de Acietos')
+    plt.title("Frecuencia de Aciertos con Capital Infinito")
+    plt.plot(frecInf,color="r")
     plt.show()
+
 
 
 def main():
     apuesta_dinero()
-    apuesta_n_veces()
-    plot_caja()
+    apuesta_dineroInf()
+    graficaDinero()
 
 
 main()
