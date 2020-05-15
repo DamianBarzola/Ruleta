f5892a7ff719ad64634c05113435a5197447557e
Damian <dbdamix@gmail.com>
2020-05-15 18:00:07 -0300

    asd

--- TP2.1.py
+++ TP2.1.py
@@ -1,36 +1,71 @@
 #Se pide programar al menos dos generadores de números pseudoaleatorios en particular el generador GCL del cual
 #se debe testear con al menos cuatro pruebas para determinar la calidad de generación. También se pide comparar los
-#generadores programados con otros, incluyendo el que posee el lenguaje Python.
-#A modo de introducción y ejemplos de código se deja un link en la sección de Recursos online obligatorios
+#generadores programados con otros, incluyendo el que posee el lenguaje Python
 
 #https://tereom.github.io/est-computacional-2018/numeros-pseudoaleatorios.html
 
-#Algunos metodos en video
-#https://www.youtube.com/watch?v=2EqFYmpCnm8
-import sys
+import numpy as np
+import random as rand
+
+
+semilla = 0
+
+
+def GenMedio(intervalo):
+    x = semilla
+    if len(str(x)) > 4:
+        x = x[:4]
+    while len(str(x)) < 4:
+        x = int(str(x)+"1")
+    while True:
+        square = x * x
+        if len(str(square)) < 8:
+            need = 8 - len(str(square))
+            addZero = ""
+            for j in range(0,need):
+                addZero = str(0) + addZero
+            square = addZero + str(square)
+        else:
+            square = str(square)
+        x = int(square[2] + square[3] + square[4] + square[5])
+        yield x
+
+
+def GenGCL( intervalo):
+    a, b = intervalo[0], intervalo[1]
+    # parameters as in GNU C Library
+    a = 54321 #Multiplicador
+    c = 44498 #Incremento
+    m = 2**32 #Modulo
+    xi = semilla
+    while True:
+        xf = (a * xi + c) % m
+        xi = xf
+        yield int((b - a) * (xf / (m - 1)) + a)
+
+
+def muestraGCL(n, intervalo):
+    lista = []
+    gen = GenGCL(intervalo)
+    for i in range(n):
+        sig = next(gen)
+        lista.append(int(sig))
+    return lista
+
+
+def muestraMedio(n, intervalo):
+    lista = []
+    gen = GenMedio(intervalo)
+    for i in range(n):
+        sig = next(gen)
+        lista.append(int(sig))
+    return lista
 
 
 def main():
-    pass
-
-
-semillas=[]
-valor=[]
-def centrodelcuadrado(semilla,n):       #esta mal
-    semillas.append(n)
-    valor.append(n)
-    for a in range(0,n):
-        x=semilla*semilla
-        if(sys.getsizeof(int(x))<2):
-            x=(x/1e2)%1e4
-        else:
-            x=0
-        valor.append(x)
-        semillas.append(semilla)
-    return valor
-
-
-
+    rango = [1, 100]
+    print(muestraGCL(50,rango))
+    print(muestraMedio(50,rango))
 
 
 main()
